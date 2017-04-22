b4bf4652286eab2b20ee15e1a7808ccf81fc7c1f
Krystal <e.krystal@gmail.com>
2017-04-22 17:45:45 -0400

    Fixed Class Name

--- RecipeApplication/src/modFrame.java
+++ RecipeApplication/src/modFrame.java
@@ -9,12 +9,12 @@
  *
  * @author iimax
  */
-public class modFrame extends javax.swing.JFrame {
+public class ModFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form modFrame
      */
-    public modFrame() {
+    public ModFrame() {
         initComponents();
     }
 
@@ -206,20 +206,21 @@ public class modFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(modFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(modFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(modFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(modFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new modFrame().setVisible(true);
+                new ModFrame().setVisible(true);
             }
         });
     }
