8f8ca2385d22347cc0e9130d56f8425d2ea53b81
Krystal <e.krystal@gmail.com>
2017-04-22 17:45:15 -0400

    Added Dynamic Panel

--- RecipeApplication/src/Client.java
+++ RecipeApplication/src/Client.java
@@ -2,7 +2,11 @@ import java.sql.*;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.accessibility.Accessible;
+import javax.swing.JComponent;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
